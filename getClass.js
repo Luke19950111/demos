34f5ef2ce6503847c0dc65d5eef4e70babfab6fa
Luke <lukelovemoney@gmail.com>
2018-04-10 16:32:41 +0800

    封装函数getSiblings

---
+++ getClass.js
@@ -0,0 +1,10 @@
+function getClass(node,classes){
+    for(var key in classes){
+      var value = classes[key]
+      if(value){
+        node.classList.add(key)
+      }else{
+        node.classList.remove(key)
+      }
+    }
+  }
