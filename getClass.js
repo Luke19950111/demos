5f91b6ccf2ffebb10383a58cf283cade124aa1f2
Luke <lukelovemoney@gmail.com>
2018-04-10 16:58:00 +0800

    封装函数getClass

--- getClass.js
+++ getClass.js
@@ -8,3 +8,18 @@ function getClass(node,classes){
       }
     }
 }
+
+//优化
+function getClass(node,classes){
+    for(var key in classes){
+      var value = classes[key]
+      
+      var methodName = value ? 'add' : 'remove'
+      node.classList[methodName](key)
+      /*if(value){
+        node.classList.add(key)
+      }else{
+        node.classList.remove(key)
+      }*/
+    }
+}
