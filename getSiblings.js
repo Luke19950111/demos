39e31dbbe5521c5dfa7943f82d0de1a8be859ad9
Luke <lukelovemoney@gmail.com>
2018-04-10 16:35:04 +0800

    封装函数getSiblings

---
+++ getSiblings.js
@@ -0,0 +1,12 @@
+function getSiblings(node){
+    var allChildren = node.parentNode.children;
+    
+    var array = {length: 0};
+    for(let i=0; i<allChildren.length; i++){
+      if(allChildren[i] !== node){
+        array[array.length] = allChildren[i];
+        array.length += 1;
+      }
+    }
+    return array;
+  }
